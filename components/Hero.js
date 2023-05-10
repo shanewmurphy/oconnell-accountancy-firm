dfdf29c9c5214136e847a2f168075d75af6b6bd0
shanewmurphy <shn.murphy@gmail.com>
2023-05-10 19:03:54 +0100

    services section

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 export default function Hero() {
   return (
-    <div className="flex lg:flex-row md:flex-col sm:flex-col xs:flex-col justify-between mt-12 mb-60 gap-20">
+    <div className="flex lg:flex-row md:flex-col sm:flex-col xs:flex-col justify-between mt-12 lg:mb-60 md:mb-10 gap-20">
       <div className="xl:basis-8/12 lg:basis-6/12 md:basis-8/12">
-        <h1 className="xl:text-5xl lg:text-4xl md:text-5xl sm:text-3xl text-width-hero-h1 text-beige font-bold leading-10 antialiased mb-3">
+        <h1 className="xl:text-5xl lg:text-4xl md:text-5xl sm:text-3xl text-beige font-bold leading-10 antialiased lg:mb-3 md:mb-4">
           Maximise Your Financial Potential with Expert Accounting and Tax
           Consulting
         </h1>
-        <h2 className="xl:text-3xl lg:text-2xl md:text-xl sm:text-xl xl:w-9/12  text-white font-semibold antialiased">
+        <h2 className="xl:text-3xl lg:text-2xl md:text-3xl sm:text-xl xl:w-9/12  text-white font-semibold antialiased md:mb-12">
           Get Expert Tax Advice from the Comfort of Your Own Home
         </h2>
         <div className="lg:mt-8 md:mt-4 sm:mt-8 sm:mb-16">
@@ -16,7 +16,7 @@ export default function Hero() {
           </button>
         </div>
       </div>
-      <div className="xl:basis-4/12 lg:basis-4/12 md:basis-3/12">
+      <div className="xl:basis-4/12 lg:basis-4/12 md:basis-3/12 tablet_display">
         <div className="float-right">
           <Image
             className="hero_conatiner_img"
